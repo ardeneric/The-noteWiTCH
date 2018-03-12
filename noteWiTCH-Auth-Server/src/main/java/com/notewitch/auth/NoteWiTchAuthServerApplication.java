28d3a7147d7257950b61e819b4f50a4810d21e23
Eric Arden <ericar@stlghana.com>
2018-03-12 17:14:01 +0000

    ongoing

--- noteWiTCH-Auth-Server/src/main/java/com/notewitch/auth/NoteWiTchAuthServerApplication.java
+++ noteWiTCH-Auth-Server/src/main/java/com/notewitch/auth/NoteWiTchAuthServerApplication.java
@@ -2,6 +2,8 @@ package com.notewitch.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class NoteWiTchAuthServerApplication {
@@ -9,4 +11,9 @@ public class NoteWiTchAuthServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NoteWiTchAuthServerApplication.class, args);
 	}
+	
+	 @Bean
+     RestTemplate restTemplate() {
+        return new RestTemplate();
+     }
 }
