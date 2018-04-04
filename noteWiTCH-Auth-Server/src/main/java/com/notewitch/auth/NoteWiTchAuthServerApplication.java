1fd171fbc132119c9026cfe024c82b03a50b21e8
Eric Arden <ericar@stlghana.com>
2018-04-04 16:08:05 +0000

    zuul proxy security almost done

--- noteWiTCH-Auth-Server/src/main/java/com/notewitch/auth/NoteWiTchAuthServerApplication.java
+++ noteWiTCH-Auth-Server/src/main/java/com/notewitch/auth/NoteWiTchAuthServerApplication.java
@@ -2,12 +2,10 @@ package com.notewitch.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class NoteWiTchAuthServerApplication {
 
 	public static void main(String[] args) {
