2d81a62e973ebf171043d4fe907c651c037f4af8
Eric Arden <ericar@stlghana.com>
2018-04-04 17:30:22 +0000

    ongoing

--- noteWiTCH-Eureka-Service/src/main/java/com/notewitch/witch/config/OauthConfig.java
+++ noteWiTCH-Eureka-Service/src/main/java/com/notewitch/witch/config/OauthConfig.java
@@ -12,9 +12,9 @@ public class OauthConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.antMatcher("/**")
+		http.antMatcher("/***")
 			.authorizeRequests()
-			.antMatchers("/" , "/login**")
+			.antMatchers("/" , "/login**", "/eureka")
 			.permitAll()
 			.anyRequest()
 			.authenticated();
