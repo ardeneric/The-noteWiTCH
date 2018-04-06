183d93d546a1f253d1b8679dfd86a7f98428e88d
Eric Arden <ericar@stlghana.com>
2018-04-06 16:33:11 +0000

    frontend ongoing

--- NoteWiTCH-FrontEnd/src/main/java/com/notewitch/NoteWiTCHFrontEnd.java
+++ NoteWiTCH-FrontEnd/src/main/java/com/notewitch/NoteWiTCHFrontEnd.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NoteWiTchClientAppApplication {
+public class NoteWiTCHFrontEnd {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NoteWiTchClientAppApplication.class, args);
+		SpringApplication.run(NoteWiTCHFrontEnd.class, args);
 	}
+
+	
 }
