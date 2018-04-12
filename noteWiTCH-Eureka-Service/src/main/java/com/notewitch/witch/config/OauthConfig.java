86d98df56aa8bc1ad255d84f655d5738401c6b9f
Eric Arden <ericar@stlghana.com>
2018-04-12 18:18:23 +0000

    chaff removed

--- noteWiTCH-Eureka-Service/src/main/java/com/notewitch/witch/config/OauthConfig.java
+++ noteWiTCH-Eureka-Service/src/main/java/com/notewitch/witch/config/OauthConfig.java
@@ -12,12 +12,10 @@ public class OauthConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.antMatcher("/**")
-		.authorizeRequests()
-		.antMatchers("/" , "/login**")
-		.permitAll()
-		.anyRequest()
-		.authenticated();
+		http
+			.authorizeRequests()
+			.anyRequest()
+			.authenticated();
 	}
 	
 }
