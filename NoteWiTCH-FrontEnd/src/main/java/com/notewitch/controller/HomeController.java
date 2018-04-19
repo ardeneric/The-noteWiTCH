2df5cc3f633a80e136e6ff153768d4e4e5ede1c5
Eric Arden <ericar@stlghana.com>
2018-04-19 17:48:57 +0000

    working on CORS

--- NoteWiTCH-FrontEnd/src/main/java/com/notewitch/controller/HomeController.java
+++ NoteWiTCH-FrontEnd/src/main/java/com/notewitch/controller/HomeController.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
-	@GetMapping("/")
+	@GetMapping({"/","index"})
 	public String home() {
 		return "index";
+	}
+	
+	@GetMapping("/groups")
+	public String groups() {
+		return "groups";
+	}
+	
+	@GetMapping("/projects")
+	public String projects() {
+		return "projects";
 	}
 }
--- noteWiTCH-Eureka-Service/src/main/java/com/notewitch/witch/config/OauthConfig.java
+++ noteWiTCH-Eureka-Service/src/main/java/com/notewitch/witch/config/OauthConfig.java
@@ -1,9 +1,15 @@
 package com.notewitch.witch.config;
 
+import java.util.Arrays;
+
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @EnableOAuth2Sso
@@ -12,12 +18,23 @@ public class OauthConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
+		http.cors().and()
+			.csrf().disable()
 			.authorizeRequests()
 			.antMatchers("/eureka/**")
 			.permitAll()
 			.anyRequest()
 			.authenticated();
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:8761"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 	
 }
